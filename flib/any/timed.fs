2b1b7ae04f5ad4fffd7f70e273a9c5c99927287d
Werner Urech <info@itsu.ch>
2017-05-20 00:17:24 +0200

    Update flib.

--- flib/any/timed.fs
+++ flib/any/timed.fs
@@ -40,43 +40,43 @@ MAX-TIMED 4 * cells buffer: timed-data
 : timed-exec ( slot# -- )
   >r     r@ tmd-call-addr @ execute
   millis r@ tmd-last-addr !
-  r@ tmd-repe-addr @ NOT IF
+  r@ tmd-repe-addr @ not if
     \ clear callback if no repetition needed
     false r@ tmd-call-addr !
-  THEN 
+  then 
   r> drop 
 ;
 
 \ Check if a timer needs to be executed (checks if enabled and enough time passed)
 : needs-run? ( slot# -- flag )
-  dup tmd-call-addr @ IF 
+  dup tmd-call-addr @ if 
     millis over tmd-last-addr @ -  ( slot# time_since_last_run )
            over tmd-inte-addr @ >  ( slot# true_if_time_to_run )
-  ELSE
+  else
     false
-  THEN nip ;
+  then nip ;
 
 \ Check and execute all the timers
 : timed-run ( -- #exec )
   0 \ return number of executed tasks
-  MAX-TIMED 0 DO
-  i needs-run? IF
+  MAX-TIMED 0 do
+  i needs-run? if
     i timed-exec 1+
-  THEN
-LOOP ;
+  then
+loop ;
 
 \ Go to sleep if we're the only task running
 : sleep-if-alone ( -- )
-  eint? IF \ Only enter sleep mode if interrupts have been enabled
-    dint up-alone? IF sleep THEN eint
-  THEN ;
+  eint? if \ Only enter sleep mode if interrupts have been enabled
+    dint up-alone? if sleep then eint
+  then ;
 
 \ Task which handles the timers in background
 task: timedtask
 : timed& ( -- )
   timedtask activate
     begin
-      timed-run NOT IF sleep-if-alone THEN
+      timed-run not if sleep-if-alone then
       pause
     again
 ;
@@ -93,15 +93,17 @@ task: timedtask
 : call-every ( callback interval slot# -- ) true  swap call-internal ; 
 : call-never ( slot# -- ) tmd-call-addr 0 swap !  ;
 
-\ Show all timers
-: timed. ( -- ) cr
+: show1timer ( n -- )
+    ." Timer #" dup .
+    ." Interval: " dup tmd-inte-addr @ .
+    ." Last-Run: " dup tmd-last-addr @ .
+    ." Callback: " dup tmd-call-addr @ .
+    ." Repeat: "       tmd-repe-addr @ . ;
+
+: timed. ( -- )  \ Show all timers
   MAX-TIMED 0 do
-    ." Timer #" i .
-    ." Interval: " i tmd-inte-addr @ .
-    ." Last-Run: " i tmd-last-addr @ .
-    ." Callback: " i tmd-call-addr @ .
-    ." Repeat: "   i tmd-repe-addr @ .
-  cr loop ;
+    cr i show1timer
+  loop ;
 
 \ Initializes timed-data and starts multitasking
 : timed-init ( -- )
